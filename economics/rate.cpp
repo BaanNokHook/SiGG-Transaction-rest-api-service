0cb598d7fefaf7b186235dec0ecc029bde0b2d1c
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-22 21:30:41 +0700

    SiGG-Transaction-rest-api-service-rate.cpp

---
+++ economics/rate.cpp
@@ -0,0 +1,49 @@
+/**
+ * @file rate.cpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <esl/economics/rate.hpp>
+
+/*
+#ifdef WITH_PYTHON
+#include <boost/python.hpp>
+
+using namespace boost::python;
+
+
+
+BOOST_PYTHON_MODULE(price)
+{
+    class_<esl::economics::rate<std::int64_t>>("rate_int64",
+                                  init<std::int64_t, std::int64_t>())
+
+        .def(self == self)
+        .def(self != self)
+;
+//        .def(self <  self)
+//        .def(self <= self)
+//        .def(self >  self)
+//        .def(self >= self)
+//
+//        .def(self +  self)
+//        .def(self += self)
+//        .def(self -  self)
+//        .def(self -= self)
+//
+//        .def(self * self)
+//        .def(self * self)
+//        .def(self /  self)
+//        .def(self /= self)
+//        ;
+
+}
+
+
+#endif  // WITH_PYTHON
+ */
