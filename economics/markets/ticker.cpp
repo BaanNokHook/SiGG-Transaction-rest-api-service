f5132a1fadec96e261c208a6de4226c6de4599b2
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-22 21:56:08 +0700

    SiGG-Transaction-rest-api-service-economics-markets-change-config-module

--- economics/markets/ticker.cpp
+++ economics/markets/ticker.cpp
@@ -1,5 +1,12 @@
-//
-// Created by Maarten on 23/06/2020.
-//
+/**
+ * @file ticker.cpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
 
 #include <esl/economics/markets/ticker.hpp>
