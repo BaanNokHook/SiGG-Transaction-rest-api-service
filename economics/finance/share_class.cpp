86d641ecc9d0782246c16f321ecdfab6dfbfe9ba
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-22 16:07:03 +0700

    SiGG-Transaction-rest-api-service-economics-share_class.cpp

---
+++ economics/finance/share_class.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file share_class.cpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <esl/economics/finance/share_class.hpp>
+using namespace esl::economics::finance;
+/*
+#ifdef WITH_PYTHON
+#include <boost/python.hpp>
+
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(share)
+{
+
+    class_<share>( "share")
+        .def_readonly("rank", &share::rank)
+        .def_readonly("votes", &share::votes)
+        .def_readonly("dividend", &share::dividend)
+        .def_readonly("preference", &share::preference)
+        .def_readonly("cumulative", &share::cumulative)
+        .def_readonly("redeemable", &share::redeemable)
+        ;
+}
+
+
+#endif  // WITH_PYTHON
+
+*/
