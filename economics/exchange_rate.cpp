4d72d1e3dd475415c6a9f2d3dd8d8d92ba375269
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-22 21:15:12 +0700

    SiGG-Transaction-rest-api-service-economics-exchange_rate.cpp

---
+++ economics/exchange_rate.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file exchange_rate.cpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <esl/economics/exchange_rate.hpp>
