8c4da3ab747dab293d9a082aaad3515b6f471311
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-22 16:41:38 +0700

    SiGG-Transaction-rest-api-service-economics-markets-order_book-order.cpp

---
+++ economics/markets/order_book/order.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file order.cpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include "order.hpp"
