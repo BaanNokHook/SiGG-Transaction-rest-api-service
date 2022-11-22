722f5cfb0c621dbf8f3f54ad8e26144640d429ed
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-22 16:06:08 +0700

    SiGG-Transaction-rest-api-service-economics-security.cpp

---
+++ economics/finance/security.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file security.cpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#include <esl/economics/finance/security.hpp>
+using namespace esl::economics::finance;
+
+
+/*
+#ifdef WITH_PYTHON
+#include <boost/python.hpp>
+
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(security)
+{
+    class_<security, bases<esl::economics::asset, esl::economics::fungible>>
+        ( "security", init<esl::identity<esl::law::property>, isin>())
+        .def_readonly("code", &security::code)
+        .def("name",  &security::name)
+        ;
+}
+
+#endif  // WITH_PYTHON
+ */
