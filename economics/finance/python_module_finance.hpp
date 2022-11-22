4830977ed21c8a07a5e681e922a570d73de06ce7
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-22 16:05:11 +0700

    SiGG-Transaction-rest-api-service-economics-python_module_finance.hpp

---
+++ economics/finance/python_module_finance.hpp
@@ -0,0 +1,16 @@
+/**
+ * @file python_module_finance.hpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef MARKET_ECOLOGY_PYTHON_MODULE_FINANCE_HPP
+#define MARKET_ECOLOGY_PYTHON_MODULE_FINANCE_HPP
+
+
+
+#endif  // MARKET_ECOLOGY_PYTHON_MODULE_FINANCE_HPP
