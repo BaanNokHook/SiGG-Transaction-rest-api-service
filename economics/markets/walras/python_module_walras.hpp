5bd5965bd0b176b99d2cb0c297d30337d0659003
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-22 16:48:00 +0700

    SiGG-Transaction-rest-api-service-economics-markets-walras-python_module_walras.hpp

---
+++ economics/markets/walras/python_module_walras.hpp
@@ -0,0 +1,15 @@
+/**
+ * @file python_module_walras.hpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef ESL_PYTHON_MODULE_WALRAS_HPP
+#define ESL_PYTHON_MODULE_WALRAS_HPP
+
+
+#endif  // ESL_PYTHON_MODULE_WALRAS_HPP
