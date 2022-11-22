d75a97ef92367077f3756445791286139c0903e9
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-22 21:27:14 +0700

    SiGG-Transaction-rest-api-service-economics-owner.cpp

---
+++ economics/owner.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file owner.cpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <boost/serialization/export.hpp>
+
+#include <esl/economics/owner.hpp>
+#include <esl/data/log.hpp>
+
+
+namespace esl::law {
+
+}
+
+
+//BOOST_CLASS_TRACKING(esl::law::owner<esl::law::property>, boost::serialization::track_always)
+//;
+//BOOST_CLASS_EXPORT(esl::law::owner<esl::law::property>);
