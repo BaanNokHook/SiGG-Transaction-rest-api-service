98bfce2449df1d2ba2585f3a7615b6ca4648e960
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-22 21:16:39 +0700

    SiGG-Transaction-rest-api-service-economics-interest_rate.cpp

---
+++ economics/interest_rate.cpp
@@ -0,0 +1,11 @@
+/**
+ * @file interest_rate.cpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-22
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include <esl/economics/interest_rate.hpp>
