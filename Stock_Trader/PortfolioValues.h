bf1f851405ac2b836bd00dcc27eea45d5ea296bd
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-23 08:18:15 +0700

    SiGG-Transaction-rest-api-service-Stock_Trader-PortfolioValues.h

---
+++ Stock_Trader/PortfolioValues.h
@@ -0,0 +1,101 @@
+/**
+ * @file PortfolioValues.hpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-23
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#ifndef PortfolioValues_h
+#define PortfolioValues_h
+
+#include <string>
+
+struct Data {
+    
+    std::vector<std::string> CSVNames = {
+//        "BJAUT IB Equity",
+//        "BHFC IB Equity",
+//        "BOS IB Equity",
+//        "EXID IB Equity",
+//        "HMCL IB Equity",
+//        "MM IB Equity",
+//        "MSIL IB Equity",
+//        "MSS IB Equity",
+//        "MRF IB Equity",
+//        "TTMT IB Equity",
+//        "TTAN IB Equity",
+//        "Z IB Equity",
+//        "BRIT IB Equity",
+//        "CLGT IB Equity",
+//        "DABUR IB Equity",
+//        "GCPL IB Equity",
+//        "HUVR IB Equity",
+//        "ITC IB Equity",
+//        "MRCO IB Equity",
+//        "TGBL IB Equity",
+//        "UBBL IB Equity",
+        "BPCL IB Equity",
+//        "COAL IB Equity",
+        "HPCL IB Equity",
+//        "IOCL IB Equity",
+//        "ONGC IB Equity",
+//        "RIL IB Equity",
+//        "APHS IB Equity",
+//        "ARBP IB Equity",
+//        "CDH IB Equity",
+//        "CIPLA IB Equity",
+//        "DIVI IB Equity",
+//        "DRRD IB Equity",
+//        "GNP IB Equity",
+        "LPC IB Equity",
+//        "PIEL IB Equity",
+//        "SUNP IB Equity",
+//        "ABB IB Equity",
+//        "ADSEZ IB Equity",
+//        "AL IB Equity",
+//        "BHE IB Equity",
+//        "BHEL IB Equity",
+//        "CCRI IB Equity",
+//        "KKC IB Equity",
+//        "EIM IB Equity",
+//        "HAVL IB Equity",
+//        "LT IB Equity",
+//        "SIEM IB Equity",
+//        "HCLT IB Equity",
+//        "INFO IB Equity",
+//        "TECHM IB Equity",
+//        "WPRO IB Equity",
+//        "ACC IB Equity",
+//        "ACEM IB Equity",
+//        "APNT IB Equity",
+//        "GRASIM IB Equity",
+//        "HINDL IB Equity",
+//        "JSTL IB Equity",
+//        "NMDC IB Equity",
+//        "PIDI IB Equity",
+//        "SAIL IB Equity",
+//        "TTCH IB Equity",
+//        "TATA IB Equity",
+//        "UTCEM IB Equity",
+//        "UPLL IB Equity",
+//        "VEDL IB Equity",
+//        "DLFU IB Equity",
+//        "BHARTI IB Equity",
+//        "IDEA IB Equity",
+//        "GAIL IB Equity",
+//        "NTPC IB Equity",
+//        "PWGR IB Equity",
+//        "RELI IB Equity",
+//        "TPWR IB Equity"
+    };
+    
+    std::vector<std::vector<int>> PermutationsValues = {};
+    
+    std::string predictionDataCSV = "PredictionData";
+    
+};
+
+#endif /* PortfolioValues_h */
