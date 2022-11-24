a94ebe11ff09f5f54e4f18a2af7de3865643577d
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-24 13:00:42 +0700

    SiGG-Transaction-rest-api-service-Endpoint-RESTAPICore-EndpointRequestData.hpp

---
+++ src/RESTAPICore/Endpoint/EndpointRequestData.h
@@ -0,0 +1,54 @@
+/**
+ * @file EndpointRequestData.hpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-24 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#pragma once
+
+#include "RESTAPICore/Endpoint/EndpointRequestAuthorizationClaims.h"
+#include "RESTAPICore/Endpoint/EndpointRequestParams.h"
+
+#include "WebServerAdapterInterface/Model/RequestHeaders.h"
+#include "WebServerAdapterInterface/Model/RequestQueryStrings.h"
+
+
+namespace systelab { namespace rest_api_core {
+
+	class EndpointRequestData
+	{
+	public:
+		EndpointRequestData();
+		EndpointRequestData(const EndpointRequestParams&,
+							const std::string&,
+							const systelab::web_server::RequestHeaders&,
+							const systelab::web_server::RequestQueryStrings&,
+							const EndpointRequestAuthorizationClaims&);
+		EndpointRequestData(const EndpointRequestData&);
+		virtual ~EndpointRequestData();
+
+		const EndpointRequestParams& getParameters() const;
+		const std::string& getContent() const;
+		const systelab::web_server::RequestHeaders& getHeaders() const;
+		const systelab::web_server::RequestQueryStrings& getQueryStrings() const;
+		const EndpointRequestAuthorizationClaims& getAuthorizationClaims() const;
+
+		void setParameters(const EndpointRequestParams&);
+		void setContent(const std::string&);
+		void setHeaders(const systelab::web_server::RequestHeaders&);
+		void setQueryStrings(const systelab::web_server::RequestQueryStrings&);
+		void setAuthorizationClaims(const EndpointRequestAuthorizationClaims&);
+
+	private:
+		EndpointRequestParams m_parameters;
+		std::string m_content;
+		systelab::web_server::RequestHeaders m_headers;
+		systelab::web_server::RequestQueryStrings m_queryStrings;
+		EndpointRequestAuthorizationClaims m_authorizationClaims;
+	};
+
+}}
+
