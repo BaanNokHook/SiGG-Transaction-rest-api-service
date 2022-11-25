dece37fe719a0b0c6c4f30275d49e196b86ed89b
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-11-25 14:28:46 +0700

    SiGG-Transaction-rest-api-service-RESTFUL_web-service-Core-Domain-Shared-Entity

---
+++ src/RESTFUL_web-service-Core/Domain/Shared/Entity.cpp
@@ -0,0 +1,60 @@
+/**
+ * @file Entity.cpp
+ * @author SiGG (Project::SiGG-Transaction-rest-api-service)
+ * @brief 
+ * @version 0.1
+ * @date 2022-11-25
+ * @copyright Copyright (c) 2022
+ * 
+ */
+#include "Domain/Shared/Entity.h"
+#include "Poco/UUIDGenerator.h"
+
+namespace Domain {
+namespace Shared {
+
+
+    Entity::Entity()  
+          : _identity(Poco::UUIDGenerator().createOne().toString())   
+    {
+
+         if ( Poco::UUID(_identity).isNull() ) {
+            throw Poco::IllegalStateException{
+                  _identity + " is not a valid UUID for an Entity."   
+            };   
+         }  
+    }     
+
+    Entity::Entity(const std::string & identity)  
+        : _identity(identity)  
+
+    {
+
+       if ( _identity.length() != 36 || Poco::UUID(_identity).isNull() ) {
+            throw Poco::InvalidArgumentException(  
+                  identity + " is not a valid UUID for an Entity."  
+            );  
+       } 
+
+    }   
+
+    Entity::~Entity()  
+    { }  
+
+    bool Entity::operator==(const IEntity & other)   
+    {
+        return this == &other && identity() == other.identity();  
+    }
+
+    bool Entity::operator!=(const IEntity & other)  
+    {
+        return this != &other && identity() != other.identity();  
+    }
+
+    std::string Entity::identity() const 
+    {
+        return _identity;     
+    }
+
+   } 
+}
